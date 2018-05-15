0835c955f16cc78d555108bc39c98be294e3ee0f
Somayeh <mail.somayeh@gmail.com>
2018-05-15 18:00:29 +0100

    Use attr_reader and attr_writer

--- conditions/class_1.rb
+++ conditions/class_1.rb
@@ -1,4 +1,7 @@
 class Vehicle
+  # attr_reader :number_of_wheels
+  attr_writer :number_of_wheels
+
   def initialize(number_of_wheels, type_of_tank, seating_capacity, maximum_velocity)
     @number_of_wheels = number_of_wheels
     @type_of_tank = type_of_tank
@@ -6,14 +9,7 @@ class Vehicle
     @maximum_velocity = maximum_velocity
   end
 
-  def number_of_wheels
-    @number_of_wheels
-  end
-
-  def set_number_of_wheels=(number)
-    @number_of_wheels = number
-  end
-
 end
 
 tesla_model = Vehicle.new(4, 'electric', 5, 300)
+tesla_model.number_of_wheels

08b3d01234d78fd6bf58a43187031bb461ec121e
Somayeh <mail.somayeh@gmail.com>
2018-05-15 17:10:50 +0100

    Hashes

---
+++ conditions/hash.rb
@@ -0,0 +1,9 @@
+hash = {
+  "name" => "Sam",
+  "surname" => "Lya",
+  "age" => 24
+}
+
+print "My name is #{hash["name"]}"
+print "My surname is #{hash["surname"]}"
+print "I am #{hash["age"]} years old"
